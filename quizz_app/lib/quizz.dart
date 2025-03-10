a21e3d388db7b027fc53dc4d2c2924be744fcb87
ghost9742 <milosavljevic.nemanja87@gmail.com>
2025-03-10 14:35:14 +0100

    first commit

---
+++ quizz_app/lib/quizz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/questions_screen.dart';
+import "package:quizz_app/start_screen.dart";
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
+        ),
+      ),
+    );
+  }
+}
